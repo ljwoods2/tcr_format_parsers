38195481aa81c2d5185390b3fe94386c63cf2577
Lawson <lwoods@tgen.org>
2025-04-07 15:43:30 -0700

    create cache if not exist

--- tcr_format_parsers/data/datafiles.py
+++ tcr_format_parsers/data/datafiles.py
@@ -2,12 +2,17 @@ __all__ = ["hla_uniprot_data_cache"]
 
 from importlib import resources
 from pathlib import Path
+import csv
+
+HEADERS = ["name","sequence","accession"]
 
 _data_ref = resources.files("tcr_format_parsers.data")
+hla_uniprot_data_cache = (Path(_data_ref) / "hla_uniprot_data_cache.csv")
 
-hla_uniprot_data_cache = (
-    Path(_data_ref) / "hla_uniprot_data_cache.csv"
-).as_posix()
+if not hla_uniprot_data_cache.exists():
+    with open(hla_uniprot_data_cache, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(HEADERS)
 
 
 del resources
